5382606b36e5bac536be46f793abe2ebe7f25bb3
Rishit <rishitaggarwal2005@gmail.com>
2026-02-10 23:18:30 +0530

    Fixed build script and requirements

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# 1. Install libraries from the backend folder
+pip install -r backend/requirements.txt
 
+# 2. Go inside the backend folder to run Django commands
+cd backend
+
+# 3. Collect static files and migrate database
 python manage.py collectstatic --no-input
 python manage.py migrate
