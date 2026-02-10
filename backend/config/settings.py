d3d4527f985978c448b32a5b6864638246d1bb8c
Rishit <rishitaggarwal2005@gmail.com>
2026-02-10 22:26:27 +0530

    Prepared backend for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ffyexsk(f@&0z!u(@54d5j_ckm-85n@b#(5!e_qt5lla0wvsia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True  # Easiest way to fix "Network Error"
